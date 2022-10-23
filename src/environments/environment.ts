f53c4d7db706bd044fab3aa09db6b353f20a52c1
Saul Chipana <JosueSaul20@gmail.com>
2022-10-23 13:37:22 +0200

    init commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBtmd_muKGWlQY4P7mtSYGDzg8m0v_c_40",
+    authDomain: "study-prs01.firebaseapp.com",
+    // databaseURL: "https://study-prs01-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "study-prs01",
+    storageBucket: "study-prs01.appspot.com",
+    messagingSenderId: "465320040468",
+    appId: "1:465320040468:web:12a6d344fb20b9d4a5af01"
+  }
 };
 
 /*
